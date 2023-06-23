e59e04f074fada02ebf89b7b506d65ec0381a0d6
rfjacaranda <60395507+rfjacaranda@users.noreply.github.com>
2023-06-23 13:10:09 -0400

    Update amazoncrim2020.py

--- amazoncrim2020.py
+++ amazoncrim2020.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import streamlit as st
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 
+# Obtenha os segredos do Streamlit
+db_username = st.secrets["db_username"]
+db_password = st.secrets["db_password"]
+db_name = st.secrets["db_name"]
+connection_name = st.secrets["connection_name"]
+
 # Crie uma conexão com o banco de dados usando SQLAlchemy
-engine = create_engine('postgresql://postgres:0895@localhost:5432/crime_punicao')
+engine = create_engine(f'postgresql://{db_username}:{db_password}@/{db_name}?host=/cloudsql/{connection_name}')
 
 # Consulta ao banco de dados para obter os dados da tabela "Amazoncrim"
 query = "SELECT * FROM amazoncrim2020"
@@ -25,10 +25,3 @@ st.write('Dados sobre a violência em todos os municípios da Amazônia Legal em
 
 # Exibir a tabela de dados
 st.dataframe(df)
-
-
-# In[ ]:
-
-
-
-
