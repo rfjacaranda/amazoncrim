14f685fad1ba18c6a1739e6a62f411e6aa1df909
rfjacaranda <60395507+rfjacaranda@users.noreply.github.com>
2023-06-25 08:32:50 -0400

    Update amazoncrim2020.py

--- amazoncrim2020.py
+++ amazoncrim2020.py
@@ -10,6 +10,16 @@ db_name = st.secrets["db_name"]
 connection_name = st.secrets["connection_name"]
 
 # Crie uma conexão com o banco de dados usando SQLAlchemy
+engine = create_engine(f'postgresql+pg8000://{db_username}:{db_password}@/{db_name}?unix_sock=/cloudsql/{connection_name}/.s.PGSQL.5432')
+
+# Consulta ao banco de dados para obter os dados da tabela "Amazoncrim"
+query = "SELECT * FROM amazoncrim2019"
+df = pd.read_sql_query(query, engine)
+
+# Fechar a conexão com o banco de dados
+engine.dispose()
+
+# Crie uma conexão com o banco de dados usando SQLAlchemy
 create_engine('postgresql+pg8000://[db_username]:[db_password]@/[db_name]?unix_sock=/cloudsql/[connection_name]/.s.PGSQL.5432')
 
 # Consulta ao banco de dados para obter os dados da tabela "Amazoncrim"
